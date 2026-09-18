cb4961ea7552ba2c2da208ae8c6f5fc90f4e3607
Aakash Narukula Dev <aakashnarukula.dev@gmail.com>
2026-09-19 02:37:16 +0530

    Redesign TallyBack with studio ledger theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import '@fontsource-variable/manrope'
+import '@fontsource-variable/plus-jakarta-sans'
+import '@fontsource-variable/newsreader'
 import App from './App'
 import './styles.css'
 import './splits.css'
-import './pastel.css'
+import './studio.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
