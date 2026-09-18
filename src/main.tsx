7fc2e23e5de641b272f444745894912a8f2b6b64
Aakash Narukula Dev <aakashnarukula.dev@gmail.com>
2026-09-19 02:50:01 +0530

    Restore compact layout with studio accents

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '@fontsource-variable/plus-jakarta-sans'
 import '@fontsource-variable/newsreader'
+import '@fontsource-variable/manrope'
 import App from './App'
 import './styles.css'
 import './splits.css'
-import './studio.css'
+import './pastel.css'
+import './hybrid.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
