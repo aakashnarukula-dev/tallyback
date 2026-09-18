010ef76424c1b5fd0cecc56a6d75d5b07330689e
Aakash Narukula Dev <aakashnarukula.dev@gmail.com>
2026-09-19 02:25:16 +0530

    Redesign TallyBack with compact pastel theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import '@fontsource-variable/manrope'
 import App from './App'
 import './styles.css'
 import './splits.css'
+import './pastel.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
